1bf88f4c81aa04cf603d650024f6d85135b577a4
Fernando Bueno <fcbgomes@gmail.com>
2025-09-15 04:01:55 -0300

    feat: add userId websocket rooms

--- src/transcriptions/transcriptions.gateway.ts
+++ src/transcriptions/transcriptions.gateway.ts
@@ -3,6 +3,9 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
@@ -21,7 +24,17 @@ export class TranscriptionsGateway
     console.log(`Client disconnected: ${client.id}`);
   }
 
+  @SubscribeMessage('join')
+  handleJoin(
+    @MessageBody() data: { userId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(data.userId);
+    console.log(`Client ${client.id} joined room ${data.userId}`);
+  }
+
   sendTranscriptionUpdate(userId: string, payload: any) {
+    console.log('EMITING')
     this.server.to(userId).emit('transcriptionUpdate', payload);
   }
 }
